045ce8455ea0e5829511a50555a6decefcbc504c
Rudransh Pratap Singh <tosylfluoride@gmail.com>
2025-06-29 23:54:53 +0530

    feat: SPA Proofing the exension... looking for new video additions to DOM

--- inject.js
+++ inject.js
@@ -3,3 +3,26 @@ const videos = document.querySelectorAll("video");
 videos.forEach(video => {
   video.playbackRate = 2.5;
 });
+
+const applySpeed = (video, speed) => {
+  if (video && video.playbackRate !== speed) {
+    video.playbackRate = speed;
+  }
+};
+
+const observeNewVideos = (speed) => {
+  const observer = new MutationObserver(() => {
+    const newVideos = document.querySelectorAll("video");
+    newVideos.forEach(video => applySpeed(video, speed));
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+};
+
+const defaultSpeed = 2.5;
+videos.forEach(video => applySpeed(video, defaultSpeed));
+
+observeNewVideos(defaultSpeed);
